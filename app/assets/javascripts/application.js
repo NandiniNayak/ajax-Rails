b9452ea5ae3dd844018ab78218e8f70b14eb0292
Nandini Nayak <nandini.r.nayak@gmail.com>
2018-11-13 16:36:28 +1100

    first commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,22 +3,22 @@
 var request = new XMLHttpRequest();
 // get request from test/data
 
-request.open('GET', 'test/data');
+request.open("GET", "test/data");
 // check if it is in the ready state, which indicates server is sending data
 // request.onreadyStateChange was not changing readyState to 4, hence change it to onload
 console.log(request);
 request.send();
 request.onload = function() {
-  if((request.readyState === 4) && (request.status === 200)){
-      var modify = document.getElementsByTagName('li');
-      // update only the third li upon refresh
-      modify[2].innerHTML = request.responseText;
-      // update every li
-      // for(let i in modify) {
-      //   modify[i].innerHTML = request.responseText;
-      // }
+  if (request.readyState === 4 && request.status === 200) {
+    var modify = document.getElementsByTagName("li");
+    // update only the third li upon refresh
+    modify[2].innerHTML = request.responseText;
+    // update every li
+    // for(let i in modify) {
+    //   modify[i].innerHTML = request.responseText;
+    // }
   }
-}
+};
 
 // uncomment step1: to demostarte update of li: on refersh
 // uncomment step2: to demostarte update of div on event clcik
@@ -27,37 +27,44 @@ request.onload = function() {
 function loadAjax() {
   var request = new XMLHttpRequest();
   // get request from test/data
-  request.open('GET', 'test/data');
+  request.open("GET", "test/data");
   // check if it is in the ready state, which indicates server is sending data
   // request.onreadyStateChange was not changing readyState to 4, hence change it to onload
   console.log(request);
   request.onload = function() {
-    if((request.readyState === 4) && (request.status === 200)){
-        // console.log(request.responseText);
-        var json_data = request.responseText.split("body>")
-        console.log(json_data[1]);
-        var final_json = json_data[1].split("<")[0]
-        // console.log(final_json);
-        var items = JSON.parse(final_json);
-        // console.log(items);
+    if (request.readyState === 4 && request.status === 200) {
+      // console.log(request.responseText);
+      var json_data = request.responseText.split("body>");
+      console.log(json_data[1]);
+      var final_json = json_data[1].split("<")[0];
+      // console.log(final_json);
+      var items = JSON.parse(final_json);
+      // console.log(items);
 
-        var output = '</ul>';
-        for (var key in items){
-          output += '<li>' + items[key]+ '</li>';
-        }
-        output += '</ul>';
-        document.getElementById('update').innerHTML = output;
-        // / get the last item which is an image
-        var lastItem = items.pic
-        // console.log(lastItem);
-        // create an image
-        var img = document.createElement("img");
-        img.src = lastItem;
-        img.style.width = "500px";
-        img.style.height = "300px";
-        var image = document.getElementById('update');
-        image.appendChild(img);
+      var output = "</ul>";
+      for (var key in items) {
+        output += "<li>" + items[key] + "</li>";
+      }
+      output += "</ul>";
+      document.getElementById("update").innerHTML = output;
+      // / get the last item which is an image
+      var lastItem = items.pic;
+      // console.log(lastItem);
+      // create an image
+      var img = document.createElement("img");
+      img.src = lastItem;
+      img.style.width = "500px";
+      img.style.height = "300px";
+      var image = document.getElementById("update");
+      image.appendChild(img);
     }
-  }
+  };
   request.send();
 }
+
+//
+// sorting logic:
+// 1. click on ascending or descending button
+// 2. XMLHttpRequest
+// 3. request.open get request -> sort cintroller method to filter in ascendingor descing order bu taking the corresponding column name
+// 4. alter the icon id using jquery
